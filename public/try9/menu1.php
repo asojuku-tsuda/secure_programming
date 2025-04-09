5923847d496310f2a5072041f9ba8f3932ac3755
Hiroshi <153479708+HiroshiAso@users.noreply.github.com>
2025-04-09 08:21:32 +0000

    あｄｄ

---
+++ public/try9/menu1.php
@@ -0,0 +1,18 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Menu2</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body class="cyberpunk-bg">
+    <div class="login-box">
+
+      <p>メニュー１画面</p>
+
+    </div>
+  </body>
+</html>
